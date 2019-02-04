5fc594b344fd8c3177ef7eeedabc611fca895df3
ryan <ryantxu@gmail.com>
2019-02-03 17:38:43 -0800

    format everything

--- src/module.ts
+++ src/module.ts
@@ -1,23 +1,21 @@
 import './sass/firebase.dark.scss';
 import './sass/firebase.light.scss';
 
-
 // The pages
 import {FirebaseConfigCtrl} from './components/config';
 import {FirebaseLoginPageCtrl} from './pages/login';
 import {DebugPageCtrl} from './pages/debug';
 
 // CSS Support
-import { loadPluginCss } from 'grafana/app/plugins/sdk';
+import {loadPluginCss} from 'grafana/app/plugins/sdk';
 loadPluginCss({
   dark: 'plugins/ryantxu-firebase-app/css/firebase.dark.css',
   light: 'plugins/ryantxu-firebase-app/css/firebase.light.css',
 });
-console.log( 'IN firebase app init (module.ts)' );
+console.log('IN firebase app init (module.ts)');
 
 export {
   FirebaseConfigCtrl as ConfigCtrl,
-
   // Must match `pages.component` in plugin.json
   DebugPageCtrl,
   FirebaseLoginPageCtrl,
--- src/panel/presence/module.ts
+++ src/panel/presence/module.ts
@@ -1,21 +1,19 @@
-
-import { PanelCtrl, loadPluginCss } from 'grafana/app/plugins/sdk';
+import {PanelCtrl, loadPluginCss} from 'grafana/app/plugins/sdk';
 
 loadPluginCss({
-    dark: 'plugins/ryantxu-firebase-app/css/firebase.dark.css',
-    light: 'plugins/ryantxu-firebase-app/css/firebase.light.css',
+  dark: 'plugins/ryantxu-firebase-app/css/firebase.dark.css',
+  light: 'plugins/ryantxu-firebase-app/css/firebase.light.css',
 });
 
-
 class PresensePanelCtrl extends PanelCtrl {
-    static templateUrl = 'panel/presence/module.html';
+  static templateUrl = 'panel/presence/module.html';
 
-    /** @ngInject */
-    constructor($scope, $injector) {
-        super($scope, $injector);
+  /** @ngInject */
+  constructor($scope, $injector) {
+    super($scope, $injector);
 
-        console.log( 'IN Presense', this );
-    }
+    console.log('IN Presense', this);
+  }
 }
 
-export { PresensePanelCtrl as PanelCtrl };
+export {PresensePanelCtrl as PanelCtrl};
